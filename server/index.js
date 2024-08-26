f8f83a4d787c820b20058696d3310a647887a778
pshreya747 <pshreya0711@gmail.com>
2024-08-27 00:09:32 +0530

    Implementation of job controller done .

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js"
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 
 dotenv.config({});
 
@@ -22,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http//localhost:5173',
+    origin:'http://localhost:5173',
     credentials:true
 }
 app.use(cors(corsOptions));
@@ -32,6 +33,7 @@ const PORT=  process.env.PORT || 3000;
 //api's
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
 
 app.listen(PORT,()=>{
     connectDB();
--- server/models/job.model.js
+++ server/models/job.model.js
@@ -16,6 +16,10 @@ const jobSchema = new mongoose.Schema({
         type:Number,
         required:true
     },
+    experienceLevel:{
+        type:Number,
+        required:true,
+    },
     location:{
         type:String,
         required:true

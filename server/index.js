7051f4a5d7e94e88787e9f5a5f7db76c2241c899
pshreya747 <pshreya0711@gmail.com>
2024-08-29 22:21:28 +0530

    Implementation of applications controller done..

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
@@ -34,6 +35,7 @@ const PORT=  process.env.PORT || 3000;
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
 
 app.listen(PORT,()=>{
     connectDB();

289761e5f721bd6e1df58bb738c439fc09dc314f
pshreya747 <pshreya0711@gmail.com>
2024-09-09 22:08:53 +0530

    Implementation of Job and Browse components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import Browse from './components/Browse'
 
 const appRouter = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const appRouter = createBrowserRouter([
   {
     path:"/jobs",
     element:<Jobs/>
+  },
+  {
+    path:"/browse",
+    element:<Browse/>
   }
 ])
 function App() {

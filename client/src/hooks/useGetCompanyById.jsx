2523c11dd5aae535f6f82e55d82a3952d7258c3f
pshreya747 <pshreya0711@gmail.com>
2024-09-17 15:24:39 +0530

    Updation of company section

---
+++ client/src/hooks/useGetCompanyById.jsx
@@ -0,0 +1,24 @@
+import { setSingleCompany } from '@/redux/companySlice'
+import { COMPANY_API_END_POINT} from '@/utils/constant'
+import axios from 'axios'
+import  { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+const useGetCompanyById = () => {
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        const fetchSingleCompany=async()=>{
+            try {
+                const res=await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setSingleCompany(res.data.company));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSingleCompany();
+    },[companyId,dispatch])
+}
+
+export default useGetCompanyById 
--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import {Company} from "../models/company.model.js";
+import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../utils/datauri.js";
 export const registerCompany = async(req,res)=>{
     try {
         const{companyName} = req.body;
@@ -72,8 +74,11 @@ export const updateCompany = async(req,res)=>{
         const {name, description, website, location}= req.body;
         const file = req.file;
         //cloudinery will be added..
+        const fileUri = getDataUri(file);
+        const cloudResponse= await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
 
-        const updateData= {name, description, website, location};
+        const updateData= {name, description, website, location,logo};
         const company = await Company.findByIdAndUpdate(req.params.id, updateData,{new:true});
 
         if(!company){
